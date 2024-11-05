18318d7a4f12bbf782094482817f7b56f7abc5b0
MameAnanas <sara.c77700@gmail.com>
2024-11-05 09:35:37 +0100

    Correction Exercice ToDoList

--- ToDoList/src/components/ToDo.jsx
+++ ToDoList/src/components/ToDo.jsx
@@ -1,16 +1,36 @@
-import viteLogo from '/vite.svg'
-import './ToDo.css'
+import viteLogo from "/vite.svg";
+import "./ToDo.css";
 
 // const DATE = new Date();
 // const dateDuJour = DATE.toLocaleString()
 
-function ToDo({todo, date}) {
-    return <>
+function ToDo({ todo, date }) {
+  return (
+    <>
       <li>
         {todo} - {date}
       </li>
     </>
-  }
+  );
+}
+
+function App() {
+  const DATE = new Date();
+
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <h2>{DATE.toLocaleString()}</h2>
+      <ul>
+        <ToDo todo={"Sortir les poubelles"} date={"07/11/2024"} />
+        <ToDo todo={"Nettoyer la cuisine"} date={"08/11/2024"} />
+        <ToDo todo={"Ranger le bureau"} date={"09/11/2024"} />
+      </ul>
+    </>
+  );
+}
+
+
 
 // function ToDo({todo}, {date}) {
 //   const dateDuJour = DATE.toLocaleString()
@@ -18,7 +38,7 @@ function ToDo({todo, date}) {
 //   return <>
 //     <h1>Tâches du jour</h1>
 //     <h2>Date du jour : {dateDuJour}</h2>
-    
+
 //     <ul>
 //       <li>Sortir les poubelles</li>
 //       <li>Nettoyer la salle de bain</li>
@@ -28,4 +48,4 @@ function ToDo({todo, date}) {
 //   </>
 // }
 
-export default ToDo
+export default App;
