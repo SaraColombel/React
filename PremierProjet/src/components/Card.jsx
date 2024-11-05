4338f69ae04a6abb1f12a88ad9bac72734986c7f
Sara Colombel <166294497+SaraColombel@users.noreply.github.com>
2024-11-05 14:10:40 +0100

    Delete PremierProjet directory

--- PremierProjet/src/components/Card.jsx
+++
@@ -1,7 +0,0 @@
-function Card(){
-    return <article className = 'card'>
-        <h2>Ma Carte</h2>
-    </article>
-}
-
-export default Card
--- PremierProjet/src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-function List(){
-    return <ul className = 'liste'>
-        <li>Tomate</li>
-        <li>Salade</li>
-        <li>Oignon</li>
-    </ul>
-  }
-
-export default List
