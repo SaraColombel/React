d468ad121464c8b7b64393862074544eb4b75448
MameAnanas <sara.c77700@gmail.com>
2024-11-05 15:05:52 +0100

    Dernières modifs

--- CardUser/src/App.jsx
+++ CardUser/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Card from "./Card";
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import Card from "./Card"
 
 const USER = [
   {
@@ -25,17 +25,17 @@ const USER = [
     email: "jokergotham@gmail.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis incidunt doloremque, adipisci iste excepturi dolores beatae. Dolore modi omnis quam quidem mollitia esse corrupti sint accusantium, eius magni! Dignissimos, dolorum.",
-  },
-];
+  }
+]
 
 function App() {
-  let count = 0;
+  let count = 0
   return (
     <>
       <h1 className="ThistleColor">Liste des Utilisateurs</h1>
       {USER.map(user => <Card key = {count ++} image={user.image} pseudo = {user.pseudo} email = {user.email} description = {user.description}/>)}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
