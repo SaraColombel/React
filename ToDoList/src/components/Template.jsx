63ce0aa2e53be983c57e2d1eece467f1f305e748
MameAnanas <sara.c77700@gmail.com>
2024-11-05 11:21:20 +0100

    Ajout Template

---
+++ ToDoList/src/components/Template.jsx
@@ -0,0 +1,31 @@
+function Template({ children }) {
+return (
+    <>
+    <header>
+        <img src="https://picsum.photos/100/100" />
+        <h1>HEADER</h1>
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        {children}
+    </main>
+
+    <footer>
+        <h1>FOOTER</h1>
+        <ul>
+            <li>Mentions légales</li>
+            <li>Politique de confidientialité</li>
+        </ul>
+    </footer>
+    </>
+);
+}
+
+export default Template
--- ToDoList/src/components/ToDo.jsx
+++ ToDoList/src/components/ToDo.jsx
@@ -1,5 +1,6 @@
 import viteLogo from "/vite.svg";
 import "./ToDo.css";
+import Template from "./Template"
 
 // const DATE = new Date();
 // const dateDuJour = DATE.toLocaleString()
@@ -19,33 +20,17 @@ function App() {
 
   return (
     <>
-      <h1>ToDo List</h1>
-      <h2>{DATE.toLocaleString()}</h2>
-      <ul>
-        <ToDo todo={"Sortir les poubelles"} date={"07/11/2024"} />
-        <ToDo todo={"Nettoyer la cuisine"} date={"08/11/2024"} />
-        <ToDo todo={"Ranger le bureau"} date={"09/11/2024"} />
-      </ul>
+      <Template>
+        <h1>ToDo List</h1>
+        <h2>{DATE.toLocaleString()}</h2>
+        <ul>
+          <ToDo todo={"Sortir les poubelles"} date={"07/11/2024"} />
+          <ToDo todo={"Nettoyer la cuisine"} date={"08/11/2024"} />
+          <ToDo todo={"Ranger le bureau"} date={"09/11/2024"} />
+        </ul>
+      </Template>
     </>
   );
 }
-
-
-
-// function ToDo({todo}, {date}) {
-//   const dateDuJour = DATE.toLocaleString()
-
-//   return <>
-//     <h1>Tâches du jour</h1>
-//     <h2>Date du jour : {dateDuJour}</h2>
-
-//     <ul>
-//       <li>Sortir les poubelles</li>
-//       <li>Nettoyer la salle de bain</li>
-//       <li>Ranger le bureau</li>
-//     </ul>
-
-//   </>
-// }
 
 export default App;
