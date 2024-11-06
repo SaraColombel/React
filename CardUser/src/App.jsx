3cee5481f9d2af7ea72fe6e3f93716d0df3fe20d
MameAnanas <sara.c77700@gmail.com>
2024-11-06 14:42:55 +0100

    Last modifications

--- CardUser/src/App.jsx
+++ CardUser/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-import Card from "./Card"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./Card";
 
 const USER = [
   {
@@ -11,13 +11,15 @@ const USER = [
     email: "batmangotham@gmail.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis incidunt doloremque, adipisci iste excepturi dolores beatae. Dolore modi omnis quam quidem mollitia esse corrupti sint accusantium, eius magni! Dignissimos, dolorum.",
+    sexe: "homme",
   },
   {
     image: "https://picsum.photos/200/200",
-    pseudo: "Robin",
+    pseudo: "Robin(e)",
     email: "robingotham@gmail.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis incidunt doloremque, adipisci iste excepturi dolores beatae. Dolore modi omnis quam quidem mollitia esse corrupti sint accusantium, eius magni! Dignissimos, dolorum.",
+    sexe: "femme",
   },
   {
     image: "https://picsum.photos/200/200",
@@ -25,17 +27,51 @@ const USER = [
     email: "jokergotham@gmail.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis incidunt doloremque, adipisci iste excepturi dolores beatae. Dolore modi omnis quam quidem mollitia esse corrupti sint accusantium, eius magni! Dignissimos, dolorum.",
-  }
-]
+    sexe: "non-binaire",
+  },
+];
+
+// function App() {
+//   let count = 0;
+//   return (
+//     <>
+//       <h1 className="ThistleColor">Liste des Utilisateurs</h1>
+//       {USER.map((user) => (
+//         <Card
+//           key={count++}
+//           image={user.image}
+//           pseudo={user.pseudo}
+//           email={user.email}
+//           description={user.description}
+//         />
+//       ))}
+//     </>
+//   );
+// }
 
 function App() {
-  let count = 0
-  return (
-    <>
-      <h1 className="ThistleColor">Liste des Utilisateurs</h1>
-      {USER.map(user => <Card key = {count ++} image={user.image} pseudo = {user.pseudo} email = {user.email} description = {user.description}/>)}
-    </>
-  )
+  let count = 0;
+  if (USER.length > 0) {
+    return (
+      <>
+        <h1 className="ThistleColor">Liste des Utilisateurs</h1>
+        <p>Il y a {USER.length} utilisateurs inscrits !</p>
+        <div class="center">
+          {USER.map((user) => (
+            <Card
+              key={count++}
+              image={user.image}
+              pseudo={user.pseudo}
+              email={user.email}
+              description={user.description}
+              sexe={user.sexe}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
+  return <h1 className="ThistleColor">Aucun utilisateur d'inscrit</h1>;
 }
 
-export default App
+export default App;
--- CardUser/src/Card.jsx
+++ CardUser/src/Card.jsx
@@ -1,7 +1,7 @@
 import './Card.css'
 
-function Card({image, pseudo, email, description}){
-    return <article className = 'card'>
+function Card({image, pseudo, email, description, sexe}){
+    return <article className = {sexe}>
         <img src = {image}/>
         <h2>{pseudo}</h2>
         <h3>{email}</h3>
