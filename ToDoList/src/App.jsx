5d59edde0b26bca0ada24573d23d9b624c400679
MameAnanas <sara.c77700@gmail.com>
2024-11-06 13:42:36 +0100

    Modification

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,20 +1,25 @@
-import ToDo from './components/ToDo'
+import ToDoTernaire from './components/ToDo'
 import Template from "./components/Template"
+import "./App.css"
 
 const TODOS = [
     {
         todo : 'Sortir les poubelles',
-        date : '07/11/2024'
+        date : '07/11/2024',
+        checked : false
     },
     {
         todo : 'Nettoyer la cuisine',
-        date : '08/11/2024'
+        date : '08/11/2024',
+        checked : false
     },
     {
         todo : 'Ranger le bureau',
-        date : '09/11/2024'
+        date : '09/11/2024',
+        checked : true
     }
 ]
+console.log(TODOS);
 
 function App() {
     const DATE = new Date();
@@ -25,7 +30,7 @@ function App() {
         <h1>To-Do List</h1>
         <h3>Date du jour : {DATE.toLocaleString()}</h3>
         <ul>
-            {TODOS.map(todo => <ToDo key = {count++} todo={todo.todo} date={todo.date} />)}
+            {TODOS.map(todo => <ToDoTernaire key = {count++} todo={todo.todo} date={todo.date} checked={todo.checked}/>)}
         </ul>
         </Template>
     )
--- ToDoList/src/components/ToDo.jsx
+++ ToDoList/src/components/ToDo.jsx
@@ -1,14 +1,37 @@
-import viteLogo from "/vite.svg"
 import "./ToDo.css"
 
 // const DATE = new Date();
 // const dateDuJour = DATE.toLocaleString()
 
-function ToDo({ todo, date }) {
-  return (
-      <li>
-        {todo} - {date}
-      </li>
-  );}
+// -------------- IF CONDITION -------------
+// function ToDo({todo, date, checked}) {
+//   if(checked){
+//     return <li className = "green"><input type="checkbox" defaultChecked></input>{todo} - {date}</li> }
+//   return <li className = "orange">{todo} - {date}</li>
+// }
+// export default ToDo
 
-export default ToDo
+
+// ------------- TERNAIRE CONDITION -----------
+// function ToDoTernaire({todo, date, checked}){
+//     return checked ?
+//     <li className = "green"><input type="checkbox" defaultChecked></input>{todo} - {date}</li> : <li className = "orange">{todo} - {date}</li>
+// }
+// export default ToDoTernaire
+
+
+// ---------------- AND CONDITION && -------------
+// function ToDoAnd({todo, date, checked}){
+//   return <li>{todo} - {date}{checked && <input type="checkbox" defaultChecked></input>}</li>
+// }
+
+  // function ToDo({ todo, date, checked}) {
+  //   return (
+  //       <li>
+  //         {todo} - {date}
+  //       </li>
+  //   );}
+  // export default ToDoAnd
+
+
+
