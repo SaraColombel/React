eb5d9c12e750fdc9d0e023ced43c8fd19dc1e275
MameAnanas <sara.c77700@gmail.com>
2024-11-07 15:04:34 +0100

    Ajout d'un filtre et de catégories

--- CardUser/src/Card.jsx
+++ CardUser/src/Card.jsx
@@ -1,37 +1,18 @@
 import "./Card.css";
 
 function Card({ image, pseudo, email, description, sexe }) {
-  return (
+    return (
     <article
-      className={sexe}
-      onMouseOver={(event) => event.target.classList.add("border")}
-      onMouseOut={(event) => event.target.classList.remove("border")}
+        className={sexe}
+        onMouseEnter={(event) => event.target.classList.add("border")}
+        onMouseOut={(event) => event.target.classList.remove("border")}
     >
-      <img
-        src={image}
-        onMouseOver={(event) => event.stopPropagation()}
-        onMouseOut={(event) => event.stopPropagation()}
-      />
-      <h2
-        onMouseOver={(event) => event.stopPropagation()}
-        onMouseOut={(event) => event.stopPropagation()}
-      >
-        {pseudo}{" "}
-      </h2>
-      <h3
-        onMouseOver={(event) => event.stopPropagation()}
-        onMouseOut={(event) => event.stopPropagation()}
-      >
-        {email}
-      </h3>
-      <p
-        onMouseOver={(event) => event.stopPropagation()}
-        onMouseOut={(event) => event.stopPropagation()}
-      >
-        {description}
-      </p>
+        <img src={image} />
+        <h2>{pseudo} </h2>
+        <h3>{email}</h3>
+        <p>{description}</p>
     </article>
-  );
+    );
 }
 
 export default Card;
